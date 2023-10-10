081eaec3f4136307c960a353698144042e06d929
fbouras-nova <farid.bouras@nova-technology.fr>
2023-10-10 10:54:09 +0200

    feat: add clear InputFilter

--- scripts/utils/clearInput.js
+++ scripts/utils/clearInput.js
@@ -1,6 +1,6 @@
 // Sélectionne l'élément input et la croix
-const input = document.getElementById('name');
-const clearInput = document.getElementById('clearInput');
+const input = document.getElementById('inputNav');
+const clearInput = document.getElementById('clearInputNav');
 
 // Afficher la croix lorsque du texte est saisi
 input.addEventListener('input', function () {
@@ -15,4 +15,23 @@ input.addEventListener('input', function () {
 clearInput.addEventListener('click', function () {
   input.value = '';
   clearInput.style.display = 'none';
+});
+
+// Sélectionne l'élément input et la croix
+const inputFilter = document.getElementById('searchInputFilter');
+const clearInputFilter = document.getElementById('clearInputFilter');
+
+// Afficher la croix lorsque du texte est saisi
+inputFilter.addEventListener('input', function () {
+  if (inputFilter.value.trim() !== '') {
+    clearInputFilter.style.display = 'inline';
+  } else {
+    clearInputFilter.style.display = 'none';
+  }
+});
+
+// Effacer le contenu du champ d'entrée lorsque la croix est cliquée
+clearInputFilter.addEventListener('click', function () {
+  inputFilter.value = '';
+  clearInputFilter.style.display = 'none';
 });
