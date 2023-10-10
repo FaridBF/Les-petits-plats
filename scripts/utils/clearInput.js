886b7d23b30af73c2bd2d9de86f8cdcd9c61e480
fbouras-nova <farid.bouras@nova-technology.fr>
2023-10-10 16:02:15 +0200

    feat: add fomating clearInput.js

--- scripts/utils/clearInput.js
+++ scripts/utils/clearInput.js
@@ -1,37 +1,20 @@
-// Sélectionne l'élément input et la croix
-const input = document.getElementById('inputNav');
-const clearInput = document.getElementById('clearInputNav');
+const cleanInput = (inputID, clearInputID) => {
+  // Sélectionne l'élément input et la croix
+  const input = document.getElementById(inputID);
+  const clearInput = document.getElementById(clearInputID);
 
-// Afficher la croix lorsque du texte est saisi
-input.addEventListener('input', function () {
-  if (input.value.trim() !== '') {
-    clearInput.style.display = 'inline';
-  } else {
+  // Afficher la croix lorsque du texte est saisi
+  input.addEventListener('input', () => {
+    if (input.value.trim() !== '') return (clearInput.style.display = 'inline');
     clearInput.style.display = 'none';
-  }
-});
+  });
 
-// Effacer le contenu du champ d'entrée lorsque la croix est cliquée
-clearInput.addEventListener('click', function () {
-  input.value = '';
-  clearInput.style.display = 'none';
-});
+  // Effacer le contenu du champ d'entrée lorsque la croix est cliquée
+  clearInput.addEventListener('click', () => {
+    input.value = '';
+    clearInput.style.display = 'none';
+  });
+};
 
-// Sélectionne l'élément input et la croix
-const inputFilter = document.getElementById('searchInputFilter');
-const clearInputFilter = document.getElementById('clearInputFilter');
-
-// Afficher la croix lorsque du texte est saisi
-inputFilter.addEventListener('input', function () {
-  if (inputFilter.value.trim() !== '') {
-    clearInputFilter.style.display = 'inline';
-  } else {
-    clearInputFilter.style.display = 'none';
-  }
-});
-
-// Effacer le contenu du champ d'entrée lorsque la croix est cliquée
-clearInputFilter.addEventListener('click', function () {
-  inputFilter.value = '';
-  clearInputFilter.style.display = 'none';
-});
+cleanInput('inputNav', 'clearInputNav');
+cleanInput('searchInputFilter', 'clearInputFilter');
