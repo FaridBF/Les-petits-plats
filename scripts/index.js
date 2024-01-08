f12171b464d782cc03f767406f896d4d90728deb
fbouras-nova <farid.bouras@nova-technology.fr>
2024-01-08 17:42:22 +0100

    refacto: index

---
+++ scripts/index.js
@@ -0,0 +1,59 @@
+import RecipeApi from './api/Api.js';
+import { fillRecipeCardTemplate, recipeCardTemplate } from './ui/recipeCard.js';
+import { cardContainer } from './ui/recipeCard.js';
+import { fillFilters } from './components/filters.js';
+
+/**
+ * ------------
+ * CONSTANTES
+ * ------------
+ */
+const recipeApi = new RecipeApi('../../data/recipes.json');
+let recipes = [];
+
+/**
+ * ------------
+ * FONCTIONS
+ * ------------
+ */
+
+/**
+ * Crée des 'card HTML' pour chaque recette et les ajoute au conteneur
+ * @param {Object[]} recipes - Liste des recettes à afficher
+ */
+function displayRecipes(recipes) {
+  const recipeCards = recipes.map((recipe) => {
+    const filledTemplate = fillRecipeCardTemplate(recipeCardTemplate, recipe);
+    const card = document.createElement('div');
+    card.innerHTML = filledTemplate;
+    return card;
+  });
+
+  // Ajoute les cartes au conteneur
+  recipeCards.forEach((card) => {
+    cardContainer.appendChild(card);
+  });
+}
+
+/**
+ * Récupère les données de recettes, crée des cartes HTML et les ajoute au conteneur.
+ * @method
+ * @returns {Promise} - Les données de recettes
+ */
+recipeApi
+  .getRecipes()
+  .then((recipesData) => {
+    recipes = recipesData;
+    if (recipes.length > 0) {
+      displayRecipes(recipes);
+      fillFilters(recipes);
+    }
+  })
+  .catch((error) => {
+    console.error(
+      "Une erreur s'est produite lors de la récupération des données de recettes :",
+      error
+    );
+  });
+
+export { displayRecipes, recipes };
