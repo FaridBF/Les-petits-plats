5abdcb0a224968fc20ad43b95fbf885e88929316
fbouras-nova <farid.bouras@nova-technology.fr>
2024-01-16 09:45:19 +0100

    feat: clear search input on filters

--- scripts/components/filtersSearchInput.js
+++ scripts/components/filtersSearchInput.js
@@ -8,6 +8,12 @@
 import { capitalizeFirstLetter } from '../utils/formatData.js';
 
 /**
+ * ------------
+ * FONCTIONS
+ * ------------
+ */
+
+/**
  * Autocomplete de chaque filtre via la recherche :
  * Parcours la liste des options et n'affiche que
  * les éléments qui correspondent à la requête de recherche
@@ -26,13 +32,45 @@ function getSearchResultsForFilter(inputId, filterItemsSelector) {
 }
 
 /**
+ * Gère l'affichage de l'icône de suppression de l'input
+ * @param {string} inputId
+ */
+function manageClearInputIcon(inputId) {
+  const searchInput = document.getElementById(inputId);
+  const clearInputIcon = document
+    .getElementById(inputId)
+    .nextElementSibling.querySelector('img.closeInputFilter');
+  const searchInputValue = searchInput.value;
+
+  clearInputIcon.style.display =
+    searchInputValue.length > 0 ? 'inline' : 'none';
+}
+
+/**
  * Ajoute l'écouteur d'évènement qui lance la recherche dans le filtre
+ * et l'écouteur d'évènement qui permet de supprimer le champs de la recherche
+ * @param {'Ingredient' | 'Appliance' | 'Ustensil'} dropdownType
  * @param {string} inputId
  * @param {string} filterItemsSelector
+ * @param {string} clearInputBtnId - Sélecteur du bouton de suppression de l'input
  */
-function listenInputSearchFilter(inputId, filterItemsSelector) {
+function listenInputSearchFilter(
+  dropdownType,
+  inputId,
+  filterItemsSelector,
+  clearInputBtnId
+) {
   const searchInput = document.getElementById(inputId);
+  const clearInputButton = document.getElementById(clearInputBtnId);
+
   searchInput.addEventListener('keyup', () => {
+    manageClearInputIcon(inputId);
+    getSearchResultsForFilter(inputId, filterItemsSelector);
+  });
+
+  clearInputButton.addEventListener('click', () => {
+    resetInputOnDropdown(dropdownType);
+    manageClearInputIcon(inputId);
     getSearchResultsForFilter(inputId, filterItemsSelector);
   });
 }
